d047232af4c3b99adab16a8ed375ae3456d95e0b
Mathias Rav <rav@cs.au.dk>
2015-12-27 11:02:06 +0100

    Extend admin.py

--- weblatex/admin.py
+++ weblatex/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from weblatex.models import Song
+from weblatex.models import Song, Booklet, BookletEntry
 
 
 admin.site.register(Song, admin.ModelAdmin)
+admin.site.register(Booklet, admin.ModelAdmin)
+admin.site.register(BookletEntry, admin.ModelAdmin)
