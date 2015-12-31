65fd3c20d434393bc74a101e4fcca037d96f915a
Mathias Rav <rav@cs.au.dk>
2015-12-31 10:44:00 +0100

    Break a line

--- weblatex/urls.py
+++ weblatex/urls.py
@@ -18,7 +18,8 @@ from django.conf.urls import url
 # from django.conf.urls import include
 from django.contrib import admin
 from weblatex.views import (
-    BookletCreate, BookletSongs, BookletRender, SongCreate, SongUpdate,
+    BookletCreate, BookletSongs, BookletRender,
+    SongCreate, SongUpdate,
     Front,
 )
 
