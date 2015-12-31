edcd742a9380b110ed2be2624e600ae40f665697
Mathias Rav <rav@cs.au.dk>
2015-12-31 11:05:14 +0100

    Add UploadedSong to admin

--- weblatex/admin.py
+++ weblatex/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from weblatex.models import Song, Booklet, BookletEntry
+from weblatex.models import Song, Booklet, BookletEntry, UploadedSong
 
 
 admin.site.register(Song, admin.ModelAdmin)
 admin.site.register(Booklet, admin.ModelAdmin)
 admin.site.register(BookletEntry, admin.ModelAdmin)
+admin.site.register(UploadedSong, admin.ModelAdmin)
